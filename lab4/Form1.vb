8f66c86996c1dc6e9ab31dfcfdc70cb38e270a61
Yerick Ramirez <yerick.ramirez@ucrso.info>
2018-05-11 13:20:56 -0600

    Comentario

---
+++ lab4/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Dim tblDatosPersonas As New DataTable
+    Dim strNombre As String
+    Dim strApellido1 As String
+    Dim strApellido2 As String
+
+
+
+    Private Sub btnLoadTxt_Click(sender As Object, e As EventArgs) Handles btnLoadTxt.Click
+
+        Dim fileReader As StreamReader
+        Dim line As String
+        'Dim persons As New ArrayList()
+
+        tblDatosPersonas.Columns.Add("Nombre")
+        tblDatosPersonas.Columns.Add("Apellido1")
+        tblDatosPersonas.Columns.Add("Apellido2")
+
+
+        fileReader = My.Computer.FileSystem.OpenTextFileReader("C:\Users\yeric\Downloads\UCR\10 Registros.txt")
+        line = fileReader.ReadLine
+
+        Do While line <> Nothing
+            Dim vector = line.Split(" ")
+            If (vector.Length > 3) Then
+
+            Else
+
+            End If
+
+            line = fileReader.ReadLine
+        Loop
+
+
+
+        'Do While line <> Nothing
+        'persons.Add(line)
+        'line = fileReader.ReadLine
+        'Loop
+
+        'For i = 0 To persons.Count - 1
+        'Dim vector = persons(i).ToString.Split(" ")
+        'If (vector.Length = 4) Then
+
+        'End If
+        'Next
+
+    End Sub
+End Class
---
+++ lab4/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("lab4")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("lab4")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b7d89c99-d7eb-47c4-8fc4-6ba074a56d1d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
